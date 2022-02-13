d858e3007fadf97335aa3434f2e52a7c333e73a4
Vergervan <krazy.vovan@mail.ru>
2022-02-13 20:41:34 +0300

    First commit

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogScriptCreator.ViewModels
+{
+    public class MainViewModel : BaseVM
+    {
+
+    }
+}
